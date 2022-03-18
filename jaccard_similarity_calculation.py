299ef6e34402609e4760f65d9bc57f6f0653eca9
Syuukakou <tyowukylin@yahoo.co.jp>
2022-03-18 13:16:12 +0900

    change file

--- jaccard_similarity_calculation.py
+++ jaccard_similarity_calculation.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 """
+JaccardDistance_SparseMatrix
+
+Calculate Jaccard distance for sparse matrix
+
+Firstly, the jaccard similarity calculation code is created by [na-o-ys (github.com)](https://github.com/na-o-ys), and referenced from [Compute all pairwise vector similarities within a sparse matrix (Python) (na-o-ys.github.io)](http://na-o-ys.github.io/others/2015-11-07-sparse-vector-similarities.html)
+
+I didn't make extensive changes to the source code, just added support for scipy.sparse.csr.csr_matrix and a little bit of other improvements.
+
+Another way to do it is here: [python - Compute Jaccard distances on sparse matrix - Stack Overflow](https://stackoverflow.com/questions/32805916/compute-jaccard-distances-on-sparse-matrix)
+---------------------------------------------------------------
 Calculate Jaccard Distance using scipy CSR or CSC sparse matrix
 1. Calcualte Jaccard Similarities firstly 
    - --> jaccard_similarity_dense
